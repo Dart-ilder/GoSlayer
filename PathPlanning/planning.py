acc249ac2f7e3ebee1e0ca0cdc2bcc0816e5eae0
Dart-ilder <Isharman@yandex.ru>
2024-12-18 21:59:50 +0300

    Planning branch start

---
+++ PathPlanning/planning.py
@@ -0,0 +1,8 @@
+def to_grid(mp, cell_size=10):
+    """Receives global map and returns a grid of the same size, with walls as 0 and empty space as 255."""
+    grid = np.full((mp.shape[0]//cell_size, mp.shape[1]//cell_size), 255)
+    for i in range(grid.shape[0]):
+        for j in range(grid.shape[1]):
+            if np.any(mp[i*cell_size:(i+1)*cell_size, j*cell_size:(j+1)*cell_size] == 0):
+                grid[i,j] = 0
+    return grid
